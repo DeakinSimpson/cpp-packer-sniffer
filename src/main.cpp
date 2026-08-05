5ae6ae51b5df573fe6558cec266b31b63f0127a1
Deakin Simpson <deakinsimpson976@gmail.com>
2026-08-05 17:29:40 +1000

    feat: added magic number reading

--- src/main.cpp
+++ src/main.cpp
@@ -11,8 +11,9 @@
 #include <sstream>
 #include <iomanip>
 
-struct PcapHeader {
-    uint32_t magicNumbers;
+struct PcapHeader 
+{
+    uint32_t magicNumber;
     uint16_t majorVersion;
     uint16_t minorVersion;
     uint32_t reserved1;
@@ -20,23 +21,46 @@ struct PcapHeader {
     uint32_t network;
 };
 
-// a char is always 1 byte so we use char instead of BTYE
-static void ReadPcapHeader(const std::string& filename, PcapHeader& pcapHeader)
+struct Packet 
 {
-    // open the file
-    std::ifstream fs;
-    fs.open(filename, std::ifstream::binary);
-    if (!fs.good()) { return; }
+    uint32_t timestampSeconds;
+    uint32_t timestampFraction;
+    uint32_t capturePacketLength;
+    uint32_t originalPacketLength;
+};
 
-    fs.read(reinterpret_cast<char*>(&pcapHeader.magicNumbers), sizeof(pcapHeader.magicNumbers));
-    fs.read(reinterpret_cast<char*>(&pcapHeader.majorVersion), sizeof(pcapHeader.majorVersion));
-    fs.read(reinterpret_cast<char*>(&pcapHeader.minorVersion), sizeof(pcapHeader.minorVersion));
-    fs.read(reinterpret_cast<char*>(&pcapHeader.reserved1), sizeof(pcapHeader.reserved1));
-    fs.read(reinterpret_cast<char*>(&pcapHeader.reserved2), sizeof(pcapHeader.reserved2));
-    fs.read(reinterpret_cast<char*>(&pcapHeader.network), sizeof(pcapHeader.network));
-
-    fs.close();
+// a char is always 1 byte so we use char instead of BTYE
+static void ReadPcapHeader(std::ifstream* fs, PcapHeader& pcapHeader)
+{
+    // cast the header of the file to 
+    fs->read(reinterpret_cast<char*>(&pcapHeader.magicNumber), sizeof(pcapHeader.magicNumber));
+    fs->read(reinterpret_cast<char*>(&pcapHeader.majorVersion), sizeof(pcapHeader.majorVersion));
+    fs->read(reinterpret_cast<char*>(&pcapHeader.minorVersion), sizeof(pcapHeader.minorVersion));
+    fs->read(reinterpret_cast<char*>(&pcapHeader.reserved1), sizeof(pcapHeader.reserved1));
+    fs->read(reinterpret_cast<char*>(&pcapHeader.reserved2), sizeof(pcapHeader.reserved2));
+    fs->read(reinterpret_cast<char*>(&pcapHeader.network), sizeof(pcapHeader.network));
 }
+
+/*
+0 = invalid
+1 = microseconds
+2 = nanoseconds
+*/
+static char GetPcapMagicType(const PcapHeader* pcapHeader)
+{
+    // magic header types
+    const uint32_t microSeconds = 2712847316;
+    const uint32_t nanoSeconds = 2712812621;
+
+    char val = '0';
+
+    if (pcapHeader->magicNumber == microSeconds) { val = '1'; }
+    if (pcapHeader->magicNumber == nanoSeconds) { val = '2'; }
+
+    return val;
+}
+
+
 
 const std::string FILE_LOCATION = "test/pcap/test-data.pcap";
 
@@ -45,20 +69,30 @@ int main()
     std::cout << "Hello World!" << std::endl;
 
     // TODO: open file into memory, then later implement more efficient method
-    // read all bytes
+
+    // opens the file into memory
+    std::ifstream fs;
+    fs.open(FILE_LOCATION, std::ifstream::binary);
+    if (!fs.good()) { return 1; }
+
+    // reader pcap header
     PcapHeader pcapHeader;
 
-    ReadPcapHeader(FILE_LOCATION, pcapHeader);
+    ReadPcapHeader(&fs, pcapHeader);
 
-    std::cout << pcapHeader.magicNumbers << std::endl;
-    // reader pcap header
-
+    std::cout << pcapHeader.magicNumber << std::endl;
+    std::cout << GetPcapMagicType(&pcapHeader);
 
     // ts seconds
     // ts microseconds
+    char timestampType = GetPcapMagicType(&pcapHeader);
 
     // get capture packet length
 
     // original packet length
 
+    // cleanup
+    fs.clear();
+
+    return 0;
 }
